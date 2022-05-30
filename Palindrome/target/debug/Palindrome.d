cd5d2fd7a114cf96df10fe304877cf1da1c14b8c
Nicole Magagnin <55962878+magagninicole@users.noreply.github.com>
2022-05-30 15:29:51 -0300

    Delete Palindrome directory

--- Palindrome/target/debug/Palindrome.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Nicole\ Magagnin\Documents\Faculdade\TCC\CursoRustParte2\Palindrome\target\debug\Palindrome.exe: C:\Users\Nicole\ Magagnin\Documents\Faculdade\TCC\CursoRustParte2\Palindrome\src\main.rs
--- Palindrome/target/debug/deps/Palindrome.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Nicole Magagnin\Documents\Faculdade\TCC\CursoRustParte2\Palindrome\target\debug\deps\Palindrome.exe: src\main.rs
-
-C:\Users\Nicole Magagnin\Documents\Faculdade\TCC\CursoRustParte2\Palindrome\target\debug\deps\Palindrome.d: src\main.rs
-
-src\main.rs:
